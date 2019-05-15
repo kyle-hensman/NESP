1e9282ef6b1d5309aadd0cdd2bf7d3743ed03e34
EliteBook <kylehensman@gmail.com>
2019-05-15 14:21:12 -0300

    Added bootstrap and created initial login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,66 @@
 import React from 'react';
 import './App.css';
 
-const Card = (props) => (
-  <div className="card" style={{ border: '1px solid gray' }}>
-    <div>top</div>
-    <div>{props.children}</div>
-    <div>bottom</div>
-  </div>
-);
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
 
-const LoginForm = () => (
-  <div className="login-form">
-    <form onSubmit={console.log('form submitted')}>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-);
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('form sumbitted!');
+    console.log('state', this.state);
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <div className="card-body my-1">
+          <form onSubmit={this.handleFormSubmit}>
+            <input
+              name="username"
+              type="text"
+              placeholder="Username"
+              className="form-control"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              className="form-control"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
+            <button type="submit" className="btn btn-primary btn-block">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="AppWrapper" style={{ textAlign: 'Center'}}>
-      <h1>Welcome</h1>
-      <Card>
-        <LoginForm />
-      </Card>
+      <h1>Welcome to NESP Account Manager</h1>
+      <div style={{ marginTop: '25vh' }}></div>
+      <LoginForm />
     </div>
   );
 }
