960ae310f804e231a37df605bd6bbdf9fe5bddd7
EliteBook <kylehensman@gmail.com>
2019-05-17 13:00:34 -0300

    swapped mocha and chai testing for jest and supertest

---
+++ server/app.test.js
@@ -0,0 +1,37 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('Check API endpoints', () => {
+  describe('GET /', () => {
+    it('should return status 200', () => {
+      return request(app).get('/').expect(200);
+    });
+  });
+  
+  describe('GET /login', () => {
+    it('should return status 200', () => {
+      return request(app).get('/login').expect(200);
+    });
+  });
+
+  describe('GET /login', () => {
+    it('should return status 200', () => {
+      return request(app).get('/login').expect(200);
+    });
+  });
+
+  describe('POST /login', () => {
+    it('should respond with json', () => {
+      return request(app)
+        .post('/login')
+        .set('Accept', 'application/json')
+        .expect(200);
+    });
+  });
+
+  describe('GET /authrequired', () => {
+    it('should return status 302', () => {
+      return request(app).get('/authrequired').expect(302);
+    });
+  });
+});
